2937c16f6c904dd99a2cf8de0b98997b0f23841a
Thomas Milotti <thomas.milotti@gmail.com>
2020-05-04 22:27:12 +0200

    Add type NonFunctionProperties and FunctionProperties

--- src/type.ts
+++ src/type.ts
@@ -58,3 +58,10 @@ export type TT$<K> = K | Promise<K>;
 export type BuildType<Types, T> = {
     [ k in keyof Types ]: T;
 };
+
+
+export type FunctionPropertyNames<T> = { [ K in keyof T ]: T[ K ] extends Function ? K : never }[ keyof T ];
+export type FunctionProperties<T> = Pick<T, FunctionPropertyNames<T>>;
+
+export type NonFunctionPropertyNames<T> = { [ K in keyof T ]: T[ K ] extends Function ? never : K }[ keyof T ];
+export type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
