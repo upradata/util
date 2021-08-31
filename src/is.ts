6c5888045e1645c8e81d4c671156b42d348ccd93
milottit <thomas.milotti@gmail.com>
2021-08-31 14:06:15 +0200

    forked here ts-util-is because of ESM modules

--- src/is.ts
+++ src/is.ts
@@ -1,16 +1,213 @@
+/* eslint-disable space-before-function-paren */
 // export * from 'ts-util-is';
-// export * is not support by rollup with commonjs :(((
-export {
-    isArray, isBase64, isBoolean, isDate, isDateValid, isDefined, isError, isFunction, isGuid, isInfinity, isNull, isNumber, isObject, isPlainObject, isRegExp, isString, isSymbol, isUndefined
-} from './node-modules-esm/ts-util-is';
+// export * is not supported by rollup with commonjs :(((
 
-import { isUndefined, isNull, isDefined } from './node-modules-esm/ts-util-is';
+// https://github.com/justinlettau/ts-util-is is using ESM, so I forked it + I added up mines
+
+/**
+ * Determines if a reference is an `Array`.
+ *
+ * @param value Reference to check.
+ */
+export function isArray(value: any): value is any[] {
+    return Array.isArray(value);
+}
+
+/**
+ * Determines if a reference is a valid base64 string.
+ *
+ * @param value Reference to check.
+ */
+export function isBase64(value: any): value is string {
+    const base64 = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/;
+
+    return isString(value) && base64.test(value);
+}
+
+/**
+ * Determines if a reference is a `Boolean`.
+ *
+ * @param value Reference to check.
+ */
+export function isBoolean(value: any): value is boolean {
+    return typeof value === 'boolean';
+}
+
+/**
+ * Determines if a reference is a `Date`.
+ *
+ * @param value Reference to check.
+ */
+export function isDate(value: any): value is Date {
+    return Object.prototype.toString.call(value) === '[object Date]';
+}
+
+/**
+ * Determines if a reference is a valid `Date`.
+ *
+ * @param value Reference to check.
+ */
+export function isDateValid(value: any): value is Date {
+    return isDate(value) && !Number.isNaN(value.getTime());
+}
+
+/**
+ * Determines if a reference is defined.
+ *
+ * @param value Reference to check.
+ */
+export function isDefined(value: any): boolean {
+    return typeof value !== 'undefined';
+}
+
+/**
+ * Determines if a reference is an `Error`.
+ *
+ * @param value Reference to check.
+ */
+export function isError(value: any): value is Error {
+    return (
+        Object.prototype.toString.call(value) === '[object Error]' ||
+        value instanceof Error
+    );
+}
+
+/**
+ * Determines if a reference is a `Function`.
+ *
+ * @param value Reference to check.
+ */
+// tslint:disable-next-line:ban-types
+export function isFunction(value: any): value is Function {
+    return typeof value === 'function';
+}
+
+/**
+ * Determines if a reference is a valid GUID string.
+ *
+ * @param value Reference to check.
+ */
+export function isGuid(value: any): value is string {
+    const guid = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+    return isString(value) && guid.test(value);
+}
+
+/**
+ * Determines if a reference is `Infinity` (positive or negative).
+ *
+ * @param value Reference to check.
+ */
+export function isInfinity(value: any): value is number {
+    return value === Infinity || value === -Infinity;
+}
+
+/**
+ * Determines if a reference is `null`.
+ *
+ * @param value Reference to check.
+ */
+export function isNull(value: any): value is null {
+    return value === null;
+}
+
+/**
+ * Determines if a reference is a `Number`.
+ *
+ * @param value Reference to check.
+ */
+export function isNumber(value: any): value is number {
+    return typeof value === 'number';
+}
+
+/**
+ * Determines if a reference is an 'Object'.
+ *
+ * @param value Reference to check.
+ */
+export function isObject(value: any): value is object {
+    return typeof value === 'object';
+}
+
+/**
+ * Determines if a reference is a plain `Object`. A "plain" object is typically created by `{}` or
+ * `new Object()`. Some types such as arrays and null, while technically objects, are not considered
+ * plain objects.
+ *
+ * @param value Reference to check.
+ */
+export function isPlainObject(value: any): value is object {
+    return (
+        isObject(value) &&
+        Object.prototype.toString.call(value) === '[object Object]'
+    );
+}
+
+/**
+ * Determines if a reference is a `RegExp`.
+ *
+ * @param value Reference to check.
+ */
+export function isRegExp(value: any): value is RegExp {
+    return Object.prototype.toString.call(value) === '[object RegExp]';
+}
+
+/**
+ * Determines if a reference is a `String`.
+ *
+ * @param value Reference to check.
+ */
+export function isString(value: any): value is string {
+    return typeof value === 'string';
+}
+
+/**
+ * Determines if a reference is a `Symbol`.
+ *
+ * @param value Reference to check.
+ */
+export function isSymbol(value: any): value is symbol {
+    return typeof value === 'symbol';
+}
+
+/**
+ * Determines if a reference is `undefined`.
+ *
+ * @param value Reference to check.
+ */
+export function isUndefined(value: any): value is undefined {
+    return typeof value === 'undefined';
+}
+
+/**
+ * Determines if a reference is null or undefined.
+ *
+ * @param value Reference to check
+ */
+export function isNil(value: any): value is null | undefined {
+    return value === null || value === undefined;
+}
+
+/**
+ * Determines if a reference is an instance of `type`.
+ *
+ * @param value Reference to check
+ * @param ctor Constructor type to check against
+ */
+export function isInstance<T extends new (...args: any[]) => any>(
+    value: any,
+    ctor: T
+): value is InstanceType<T> {
+    return value instanceof ctor;
+}
+
+
 
 
 // MDN Polyfill
 export function isInt(n: number) {
     return typeof n === 'number' &&
-        isFinite(n) &&
+        Number.isFinite(n) &&
         Math.floor(n) === n;
 }
 
@@ -18,9 +215,6 @@ export function isFloat(n: number) {
     return !isInt(n);
 }
 
-export function isNil(value: any) {
-    return isNull(value) || isUndefined(value);
-}
 
 export function isAsyncFunction(value: any) {
     return Object.prototype.toString.call(value) === '[object AsyncFunction]';
---
+++ test/is.spec.ts
@@ -0,0 +1,100 @@
+import * as util from '../src';
+
+describe('ts-util-is methods', () => {
+    it('ensure value is an array', () => {
+        expect(util.isArray([])).toEqual(true);
+    });
+
+    it('ensure value is a base64 string', () => {
+        expect(util.isBase64('AgQGCAoMDhASFA==')).toEqual(true);
+    });
+
+    it('ensure value is a boolean', () => {
+        expect(util.isBoolean(true)).toEqual(true);
+    });
+
+    it('ensure value is a date', () => {
+        expect(util.isDate(new Date())).toEqual(true);
+    });
+
+    it('ensure value is a valid date', () => {
+        expect(util.isDateValid(new Date('invalid'))).toEqual(false);
+    });
+
+    it('ensure value is defined', () => {
+        expect(util.isDefined(83)).toEqual(true);
+    });
+
+    it('ensure value is an error', () => {
+        expect(util.isError(new Error())).toEqual(true);
+    });
+
+    it('ensure value is a function', () => {
+        expect(util.isFunction(() => {
+            // empty
+        })).toEqual(true);
+    });
+
+    it('ensure value is a guid', () => {
+        expect(util.isGuid('b8161432-e903-4edc-9429-6eade52e2572')).toEqual(true);
+    });
+
+    it('ensure value is infinity', () => {
+        expect(util.isInfinity(Infinity)).toEqual(true);
+    });
+
+    it('ensure value is null', () => {
+        expect(util.isNull(null)).toEqual(true);
+    });
+
+    it('ensure value is a number', () => {
+        expect(util.isNumber(83)).toEqual(true);
+    });
+
+    it('ensure value is an object', () => {
+        expect(util.isObject({})).toEqual(true);
+    });
+
+    it('ensure value is a plain object', () => {
+        expect(util.isPlainObject({})).toEqual(true);
+    });
+
+    it('ensure value is not a plain object', () => {
+        expect(util.isPlainObject([])).toEqual(false);
+    });
+
+    it('ensure value is a regex', () => {
+        expect(util.isRegExp(/83/)).toEqual(true);
+    });
+
+    it('ensure value is a string', () => {
+        expect(util.isString('')).toEqual(true);
+    });
+
+    it('ensure value is a symbol', () => {
+        expect(util.isSymbol(Symbol(''))).toEqual(true);
+    });
+
+    it('ensure value is undefined', () => {
+        expect(util.isUndefined(undefined)).toEqual(true);
+    });
+
+    it('ensure value is null or undefined', () => {
+        expect(util.isNil(null)).toEqual(true);
+        expect(util.isNil(undefined)).toEqual(true);
+        expect(util.isNil(false)).toEqual(false);
+        expect(util.isNil(1)).toEqual(false);
+    });
+
+    it('ensure value is an instance of a type', () => {
+        const err = new Error();
+        expect(util.isInstance(err, Error)).toEqual(true);
+
+        // eslint-disable-next-line no-new-func
+        const fn = new Function();
+        expect(util.isInstance(fn, Function)).toEqual(true);
+
+        expect(util.isInstance(null, Error)).toEqual(false);
+        expect(util.isInstance(false, Error)).toEqual(false);
+    });
+});
