042005b5fb6f16987b7ef231e97110c62b8ae9ab
Thomas Milotti <thomas.milotti@gmail.com>
2022-01-07 22:30:47 +0100

    small promises fix

--- src/promise.ts
+++ src/promise.ts
@@ -2,11 +2,11 @@ import type { TT$ } from './type';
 import { arrayN } from './useful';
 
 
-export const chained$ = <T>(promises: Array<(value: T, i?: number) => TT$<T>>, init: T): Promise<T> => {
+export const chained$ = <T>(promises: Array<(value: T, i?: number) => TT$<T>>, init: T = undefined): Promise<T> => {
     return promises.reduce((current, promise, i) => current.then(v => promise(v, i)), Promise.resolve(init));
 };
 
-export const repeatChained$ = <T>(func: (value: T, i?: number) => TT$<T>, n: number, init: T): Promise<T> => {
+export const repeatChained$ = <T>(func: (value: T, i?: number) => TT$<T>, n: number, init: T = undefined): Promise<T> => {
     return chained$(arrayN(n).map(_i => func), init);
 };
 
